21529fef5655db94efe12203884218054241f376
Borislav <77554605+stockfish10@users.noreply.github.com>
2023-03-17 15:18:48 +0200

    Started library app, did some frontend

---
+++ library-application/frontend/library-app/src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import { ExploreTopBooks } from './layouts/HomePage/ExploreTopBooks';
+import { Navbar } from './layouts/NavbarAndFooter/Navbar';
+
+function App() {
+  return (
+    <div>
+      <Navbar/>
+      <ExploreTopBooks/>
+    </div>
+  );
+}
+
+export default App;
