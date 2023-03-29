68f5be6ca43f8c0c470fb4d2baa7b216b98234e6
Borislav <77554605+stockfish10@users.noreply.github.com>
2023-03-29 20:50:28 +0300

    Added two methods to Book Repo, needed for Pagination and Search.

--- library-application/backend/spring-boot-library/src/main/java/com/luv2code/springbootlibrary/repository/BookRepository.java
+++ library-application/backend/spring-boot-library/src/main/java/com/luv2code/springbootlibrary/repository/BookRepository.java
@@ -1,9 +1,18 @@
 package com.luv2code.springbootlibrary.repository;
 
 import com.luv2code.springbootlibrary.entity.BookEntity;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
 
 @Repository
 public interface BookRepository extends JpaRepository<BookEntity,Long> {
+
+    Page<BookEntity> findByTitleContaining(@RequestParam("title") String title, Pageable pageable);
+
+    Page<BookEntity> findByCategory(@RequestParam("category") String category, Pageable pageable);
 }
