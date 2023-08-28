cbf771d7cd7a671d55f4ec9a14c3ec7c930b9d85
Borislav <77554605+stockfish10@users.noreply.github.com>
2023-08-28 15:08:56 +0300

    Started creating Loans front end page.

---
+++ library-application/frontend/library-app/src/layouts/ShelfPage/components/Loans.tsx
@@ -0,0 +1,61 @@
+import { useOktaAuth } from "@okta/okta-react";
+import { useEffect, useState } from "react";
+import ShelfCurrentLoans from "../../../models/ShelfCurrentLoans";
+import { SpinnerLoading } from "../../Utils/SpinnerLoading";
+
+export const Loans = () => {
+    
+    const { authState } = useOktaAuth();
+    const [httpError, setHttpError] = useState(null);
+
+    const [shelfCurrentLoans, setShelfCurrentLoans] = useState<ShelfCurrentLoans[]>([]);
+    const [isLoadingUserLoans, setIsLoadingUserLoans] = useState(true);
+
+    useEffect(() => {
+        const fetchUserCurrentLoans = async () => {
+
+            if(authState && authState.isAuthenticated) {
+                const url = `http://localhost:8080/api/bookEntities/secure/currentloans`;
+
+                const requestOptions = {
+                    method: "GET",
+                    headers: {
+                        Authorization: `Bearer ${authState.accessToken?.accessToken}`,
+                        "Content-Type": "application/json"
+                    }
+                };
+
+                const shelfCurrentLoansResponse = await fetch(url, requestOptions);
+
+                if(!shelfCurrentLoansResponse.ok) {
+                    throw new Error("Soemthing went wrong - shelfloans!");
+                }
+
+                const shelfCurrentLoansResponseJson = await shelfCurrentLoansResponse.json();
+                setShelfCurrentLoans(shelfCurrentLoansResponseJson);
+            }
+
+            setIsLoadingUserLoans(false);
+
+        }
+        fetchUserCurrentLoans().catch((error: any) => {
+            setIsLoadingUserLoans(false);
+            setHttpError(error.message);
+        })
+        window.scrollTo(0,0);
+    }, [authState]);
+    
+    if (isLoadingUserLoans) {
+        return(<SpinnerLoading/>)
+    }
+
+    if (httpError) {
+        return(<div className="container m-5">
+            <p>
+                {httpError}
+            </p>
+        </div>)
+    }
+
+    return ();
+}
