c78cda8013c507af340d2448bdeca55ae14d4746
Borislav <77554605+stockfish10@users.noreply.github.com>
2023-09-06 19:53:16 +0300

    started working on messages from user to admin. added a query in message repo and started working on front end.

--- library-application/backend/spring-boot-library/src/main/java/com/luv2code/springbootlibrary/repository/MessageRepository.java
+++ library-application/backend/spring-boot-library/src/main/java/com/luv2code/springbootlibrary/repository/MessageRepository.java
@@ -10,4 +10,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Repository
 public interface MessageRepository extends JpaRepository<Message, Long> {
     Page<Message> findByUserEmail(@RequestParam("user_email") String userEmail, Pageable pageable);
+
+    Page<Message> findByClosed(@RequestParam("closed") boolean closed, Pageable pageable);
 }
